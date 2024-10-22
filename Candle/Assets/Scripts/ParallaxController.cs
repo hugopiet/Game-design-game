0b99040c19ddbe74d8d608667821a7e372ddf43d
GauteHestad <116973139+GauteHestad@users.noreply.github.com>
2024-10-22 17:37:19 +0200

    parallax
    made the parallax effect, and added some new assest

---
+++ Candle/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    public bool tileable;
+
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if(tileable){
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+        }
+    }
+}
